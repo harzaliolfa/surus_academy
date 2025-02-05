f6239e8aa291750517a305da605f436f3c72cff6
Olfa_Elharzalli <olfa.elharzalli@newaccess.ch>
2025-02-05 11:59:20 +0100

    home component first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { FooterComponent } from "./shared/footer/footer.component";
 import { CoursesListComponent } from "./features/courses/courses-home/courses-list/courses-list.component";
 import { CoursesHomeComponent } from "./features/courses/courses-home/courses-home.component";
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./features/home/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavBarComponent, FooterComponent , RouterOutlet],
+  imports: [NavBarComponent, FooterComponent, RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
