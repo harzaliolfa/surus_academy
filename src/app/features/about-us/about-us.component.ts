3ba0ca93a2cc0a3eb48cc9cf7fc01462ed2b8a0f
Olfa_Elharzalli <olfa.elharzalli@newaccess.ch>
2025-02-09 18:58:28 +0100

    making some fixes, adding tutors section , first version of the about us section

--- src/app/features/about-us/about-us.component.ts
+++ src/app/features/about-us/about-us.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
+  benefits = [
+    { title: "Standardization", description: "Ensures a consistent training experience worldwide." },
+    { title: "Reduced Costs", description: "No cost to reproduce materials, thanks to mobile learning." },
+    { title: "More Customization", description: "Learning is not one-size-fits-all. Tailored content improves engagement." },
+    { title: "Affordable Pricing", description: "No cost to reproduce materials, thanks to mobile learning." },
+    { title: "Learner Satisfaction", description: "High satisfaction rates ensure better retention." },
+    { title: "Multimedia Materials", description: "Video, text, and interactive content for effective learning." }
+  ];
 
 }
--- src/app/features/home/home/home.component.ts
+++ src/app/features/home/home/home.component.ts
@@ -1,15 +1,39 @@
 import { Component } from '@angular/core';
 import { CoursesListComponent } from "../../courses/courses-home/courses-list/courses-list.component";
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   imports: [
-    RouterOutlet,RouterModule
+    RouterOutlet,RouterModule,CommonModule 
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  tutors = [
+    {
+      name: "Theresa Webb",
+      role: "Planning Lead",
+      bio: "Former co-founder of Planning"
+    },
+    {
+      name: "Theresa Webb",
+      role: "Planning Lead",
+      bio: "Former co-founder of Planning"
+    },
+    {
+      name: "Theresa Webb",
+      role: "Planning Lead",
+      bio: "Former co-founder of Planning"
+    },
+    {
+      name: "Theresa Webb",
+      role: "Planning Lead",
+      bio: "Former co-founder of Planning"
+    }
+  ];
 
 }
