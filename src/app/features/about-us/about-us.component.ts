948e6a0a85d84775c5f93f189fe6b5a77badd0db
Olfa_Elharzalli <olfa.elharzalli@newaccess.ch>
2025-02-11 17:06:53 +0100

    about us fixed

--- src/app/features/about-us/about-us.component.ts
+++ src/app/features/about-us/about-us.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-about-us',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
